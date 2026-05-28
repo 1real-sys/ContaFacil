4dd3b4708d8925798d9ac1744e43b734edd3009f
1real-sys <cauan.anna@edu.ufes.br>
2026-05-27 22:56:02 -0300

    fix: validação de senha, minimo de 10 caracteres, tendo o minimo de um numero e uma letra maiuscula

--- src/main/java/dev/teamwin/contafacil/dto/login/RegisterRequestDTO.java
+++ src/main/java/dev/teamwin/contafacil/dto/login/RegisterRequestDTO.java
@@ -2,9 +2,15 @@ package dev.teamwin.contafacil.dto.login;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequestDTO (@NotBlank String username,
                                   @NotBlank @Email String email,
-                                  @NotBlank @Size(min = 6) String password) {
+                                  @NotBlank @Size(min = 10)
+                                  @Pattern(
+                                          regexp = "^(?=.*[0-9])(?=.*[A-Z]).+$",
+                                          message = "A senha deve conter ao menos 1 número e 1 letra maiúscula"
+                                  )String password) {
+
 }
