452d0f9f5670603ec233d85a1eb01722792920ec
1real-sys <cauan.anna@edu.ufes.br>
2026-06-13 15:15:41 -0300

    new: inicio da implementação do caffeine

--- src/main/java/dev/teamwin/contafacil/ContaFacilApplication.java
+++ src/main/java/dev/teamwin/contafacil/ContaFacilApplication.java
@@ -2,9 +2,12 @@ package dev.teamwin.contafacil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class ContaFacilApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(ContaFacilApplication.class, args);
