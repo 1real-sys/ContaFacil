88dc1517e85909f8f2d2fa97e60add6ef7901bd1
1real-sys <cauan.anna@edu.ufes.br>
2026-06-13 15:16:43 -0300

    new: cache implementado no conta repository

--- src/main/java/dev/teamwin/contafacil/conta/ContaRepository.java
+++ src/main/java/dev/teamwin/contafacil/conta/ContaRepository.java
@@ -1,5 +1,7 @@
 package dev.teamwin.contafacil.conta;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +11,10 @@ public interface ContaRepository extends JpaRepository<ContaDomain, Long> {
 
     Optional<ContaDomain> findByContaCorrente(String contaCorrente);
 
+    @Override
+    @CacheEvict(value = "contas", key = "#entity.user.id")
+    <S extends ContaDomain> S save(S entity);
+
+    @Cacheable(value = "contas", key = "#userId")
     List<ContaDomain> findByUserId(Long userId);
 }
