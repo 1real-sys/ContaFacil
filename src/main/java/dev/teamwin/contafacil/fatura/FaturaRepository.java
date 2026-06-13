ce37815542684c9d8df2a84fe6918ace85bd3074
1real-sys <cauan.anna@edu.ufes.br>
2026-06-13 15:41:48 -0300

    feat(fatura): adiciona cache Caffeine no FaturaRepository findByCartaoIdAndAnoAndMes com @Cacheable + @EntityGraph e unless para Optional.empty; findByCartaoIdOrderByAnoDescMesDesc com @Cacheable; save com @Caching para evict em ambas as chaves.

--- src/main/java/dev/teamwin/contafacil/fatura/FaturaRepository.java
+++ src/main/java/dev/teamwin/contafacil/fatura/FaturaRepository.java
@@ -1,5 +1,9 @@
 package dev.teamwin.contafacil.fatura;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -7,9 +11,20 @@ import java.util.Optional;
 
 public interface FaturaRepository extends JpaRepository<FaturaDomain, Long> {
 
+    @Cacheable(value = "faturas", key = "#cartaoId + '-' + #ano + '-' + #mes",
+               unless = "#result.empty")
+    @EntityGraph(attributePaths = "compras")
     Optional<FaturaDomain> findByCartaoIdAndAnoAndMes(Long cartaoId, Integer ano, Integer mes);
 
+    @Cacheable(value = "faturas", key = "#cartaoId")
     List<FaturaDomain> findByCartaoIdOrderByAnoDescMesDesc(Long cartaoId);
 
     List<FaturaDomain> findByCartaoIdAndStatus(Long cartaoId, StatusFatura status);
+
+    @Override
+    @Caching(evict = {
+        @CacheEvict(value = "faturas", key = "#entity.cartao.id + '-' + #entity.ano + '-' + #entity.mes"),
+        @CacheEvict(value = "faturas", key = "#entity.cartao.id")
+    })
+    <S extends FaturaDomain> S save(S entity);
 }
