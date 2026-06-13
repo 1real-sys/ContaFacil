b4154a28416d6182cdcb8045f6097aa57ba52fbd
1real-sys <cauan.anna@edu.ufes.br>
2026-06-13 16:04:00 -0300

    feat: adiciona cache caffeine no CartaoRepository                                                                                     findByIdContaId com @Cacheable: save refatorado com @CacheEvict por conta.id

--- src/main/java/dev/teamwin/contafacil/cartao/CartaoRepository.java
+++ src/main/java/dev/teamwin/contafacil/cartao/CartaoRepository.java
@@ -1,5 +1,7 @@
 package dev.teamwin.contafacil.cartao;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -7,7 +9,13 @@ import java.util.Optional;
 
 public interface CartaoRepository extends JpaRepository<CartaoDomain, Long> {
 
-    Optional<CartaoDomain> findByNumeroCartao(String numeroCartao);
 
+    Optional<CartaoDomain> findByNumeroCartao(String numeroCartao); // nao será cacheado, função auxiliar gerar numero pode conflitar
+
+    @Cacheable(value = "cartoes", key = "#contaId")
     List<CartaoDomain> findByContaId(Long contaId);
+
+    @Override
+    @CacheEvict(value = "cartoes", key = "#entity.conta.id")
+    <S extends CartaoDomain> S save(S entity);
 }
