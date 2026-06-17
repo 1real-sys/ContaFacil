78dff803b7f83f7665f94df30f496959bf8e3def
1real-sys <cauan.anna@edu.ufes.br>
2026-06-16 22:12:51 -0300

    add cache em ComprasCartaoRepository, EntityGraph aplicado dessa vez para impedir LazyInitializationException

--- src/main/java/dev/teamwin/contafacil/comprasCartao/ComprasCartaoRepository.java
+++ src/main/java/dev/teamwin/contafacil/comprasCartao/ComprasCartaoRepository.java
@@ -1,5 +1,8 @@
 package dev.teamwin.contafacil.comprasCartao;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -7,6 +10,9 @@ import java.util.List;
 
 public interface ComprasCartaoRepository extends JpaRepository<ComprasCartaoDomain, Long> {
 
+
+    @Cacheable(value = "comprasCartao", key = "#faturaId")
+    @EntityGraph(attributePaths = "fatura")
     List<ComprasCartaoDomain> findByFaturaIdOrderByDataCompraDesc(Long faturaId);
 
     List<ComprasCartaoDomain> findByFaturaCartaoIdAndDataCompraBetweenOrderByDataCompraDesc(
@@ -14,4 +20,8 @@ public interface ComprasCartaoRepository extends JpaRepository<ComprasCartaoDoma
             LocalDateTime dataInicio,
             LocalDateTime dataFim
     );
+
+    @Override
+    @CacheEvict(value = "comprasCartao", key = "#entity.fatura.id")
+    <S extends ComprasCartaoDomain> S save(S entity);
 }
